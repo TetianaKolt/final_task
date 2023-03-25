7b7c51123e58b6f779eedb7536f48d6ab701b6bf
Tetiana Koltunova <qaengineer.tetiana.koltunova@gmail.com>
2023-03-25 09:43:25 +0200

    Added @Steps to HomePage

--- src/main/java/framework/pages/HomePage.java
+++ src/main/java/framework/pages/HomePage.java
@@ -5,6 +5,7 @@ import static framework.helpers.Helpers.scrollToElement;
 
 import framework.components.ProductComponents;
 import framework.enums.SortByOptions;
+import io.qameta.allure.Step;
 import java.util.List;
 import org.openqa.selenium.By;
 
@@ -15,7 +16,7 @@ public class HomePage extends BasePage {
   private final By productsContainerLocator =
       By.xpath("//div[@class='products row']/div");
 
-
+  @Step("Choose 'Sort by' option")
   public HomePage chooseSortByOption(SortByOptions chooseOption) {
     scrollToElement(find(By.id("js-product-list-top")));
     find(sortButtonLocator).click();
@@ -25,6 +26,7 @@ public class HomePage extends BasePage {
     return this;
   }
 
+  @Step("Get product list")
   public List<ProductComponents> getProducts() {
     waitUntilPresent(By.xpath("//div[@class='products row']"), 10);
     scrollToElement(find(productsContainerLocator));
