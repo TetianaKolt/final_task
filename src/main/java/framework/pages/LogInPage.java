e07a1588993c759259a5f003d37ca2202061448e
Tetiana Koltunova <qaengineer.tetiana.koltunova@gmail.com>
2023-03-25 09:45:44 +0200

    Added @Steps and logs to LogInPage

--- src/main/java/framework/pages/LogInPage.java
+++ src/main/java/framework/pages/LogInPage.java
@@ -1,14 +1,18 @@
 package framework.pages;
 
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 
+@Log4j2
 public class LogInPage extends BasePage {
 
   private final By noAccountLinkLocator = By.xpath(
       "//a[@data-link-action='display-register-form']");
 
-
+  @Step("Click on 'No account'")
   public CreateAnAccountPage clickOnNoAccountLink() {
+    log.info("Click on 'No account'");
     find(noAccountLinkLocator).click();
     waitUntilPageIsLoaded();
     return new CreateAnAccountPage();
