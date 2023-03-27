cbc88ec5fc0ab3fd357ef04a121c9d2d7b0e233c
Tetiana Koltunova <qaengineer.tetiana.koltunova@gmail.com>
2023-03-27 15:43:57 +0300

    updated enums SubCategories

--- src/main/java/framework/enums/Categories.java
+++ src/main/java/framework/enums/Categories.java
@@ -1,5 +1,8 @@
 package framework.enums;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -16,12 +19,16 @@ public enum Categories {
   @AllArgsConstructor
   public enum SubCategories {
     CLOTHES_SUB_CATEGORIES(
-        "//li[@id='category-3']//a[@class='dropdown-item dropdown-submenu']"),
+        "//li[@id='category-3']//a[@class='dropdown-item dropdown-submenu']",
+        Arrays.asList("MEN", "WOMEN")),
     ACCESSORIES_SUB_CATEGORIES(
-        "//li[@id='category-6']//a[@class='dropdown-item dropdown-submenu']"),
+        "//li[@id='category-6']//a[@class='dropdown-item dropdown-submenu']",
+        Arrays.asList("STATIONERY", "HOME ACCESSORIES")),
     ART_SUB_CATEGORIES(
-        "//*[@id='category-9']//a[@class='dropdown-item dropdown-submenu']");
+        "//*[@id='category-9']//a[@class='dropdown-item dropdown-submenu']",
+        Collections.emptyList());
 
     private final String subLocators;
+    private final List<String> subCategoriesNames;
   }
 }
